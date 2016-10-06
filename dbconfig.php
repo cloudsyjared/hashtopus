6846dcff1614b9100286baf999a89bc7f51046c8
Jensen <jjensen@GAM5YG3QC-MAC.hsd1.ga.comcast.net>
2016-10-06 01:05:14 -0400

    Setting up db

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
-$dbhost="localhost";
-$dbuser="dbuser";
-$dbpass="dbpass";
-$dbname="db";
+$dbhost="hashtopus-mysql";
+$dbuser="root";
+$dbpass="";
+$dbname="hashtopus";
 $dblink = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Error " . mysqli_connect_error($dblink));
 ?>
